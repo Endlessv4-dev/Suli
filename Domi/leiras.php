bb6b158cb18932d132ffae8f13b845c27ab6d34f
kdominik5 <kerteszdomi@gmail.com>
2026-04-21 18:42:13 +0200

    leiras alap megvan, mas nem nagyon

--- Domi/leiras.php
+++ Domi/leiras.php
@@ -5,8 +5,26 @@
 
     $found_items = $conn->query("SELECT * FROM items WHERE description != '-' ORDER BY RAND() LIMIT 1");
     $item = $found_items->fetch_assoc();
-
-    echo $item['name']."<br>".$item['tier']."<br>".$item['description'];
-
+    echo "<div class='descript'>";
+    echo "  <br><span class='tier'>".$item['tier']."</span><br><span class='desc'>".$item['description']."</span><br><br>";
+    if(isset($_POST['guess'])){
+        if($_POST['guess'] == $item['name']){
+            echo "Correct!<br> The item was Heartsteel";
+        }
+        else{
+            echo "Incorrect, try again!";
+        }
+    }
+    echo "</div>";
+    
+    
 
 ?>
+<br>
+<link rel="stylesheet" href="css/style.css">
+<div class="descript">
+    <form method="post">
+        <input type="text" name="guess" placeholder="Start Typing...">
+        <button type="button" name="guess-btn" onclick="this.form.submit()">Submit</button>
+    </form>
+</div>
