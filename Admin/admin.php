fea27b4acc4b322d9097f571a5fac7c87a99fac1
kdominik5 <kerteszdomi@gmail.com>
2026-05-12 19:50:54 +0200

    statok.php elkezdve xd

--- Admin/admin.php
+++ Admin/admin.php
@@ -28,7 +28,7 @@
         if (move_uploaded_file($tmp_icon_file, $path_icon)) {
             $audio_db_value = "-";
 
-            if (!empty($audio_file) && $_FILE['upload-audio']['error' === UPLOAD_ERR_OK]) {
+            if (!empty($audio_file) && $_FILES['upload-audio']['error' === UPLOAD_ERR_OK]) {
                 $path_audio = $folder."\\".$audio_file;
                 if (move_uploaded_file($tmp_audio_file, $path_audio)) {
                     $audio_db_value = $audio_file;
@@ -52,7 +52,7 @@
 
             for ($i = 0; $i < count($names); $i++) {
                 if (!empty($names[$i]) && !empty($values[$i]) && !empty($types[$i])) {
-                    $conn->query("INSERT INTO item_stats VALUES ($item[id], '$names[$i]', $values[$i], '$types[$i]')");
+                    $conn->query("INSERT INTO item_stats VALUES($item[id], '$names[$i]', $values[$i], '$types[$i]')");
                 }
             }
         }
--- Domi/leiras.php
+++ Domi/leiras.php
@@ -1,12 +1,15 @@
+<br>
+<link rel="stylesheet" href="css/style.css">
 <?php
 
     require "../Connection/config.php";
     require "../Functions/message.php";
 
-    $found_items = $conn->query("SELECT * FROM items WHERE description != '-' ORDER BY RAND() LIMIT 1");
+    $inquiry = "SELECT * FROM items WHERE description != '-' ORDER BY RAND() LIMIT 1";
+    $found_items = $conn->query($inquiry);
     $item = $found_items->fetch_assoc();
     echo "<div class='descript'>";
-    echo "  <br><span class='tier'>".$item['tier']."</span><br><span class='desc'>".$item['description']."</span><br><br>";
+    echo "  <br>".$item['tier']."<br>".$item['description']."<br><br>";
     if(isset($_POST['guess'])){
         if($_POST['guess'] == $item['name']){
             echo "Correct!<br> The item was Heartsteel";
@@ -20,10 +23,9 @@
     
 
 ?>
-<br>
-<link rel="stylesheet" href="css/style.css">
+
 <div class="descript">
-    <form method="post">
+    <form method="post" class='desc-form'>
         <input type="text" name="guess" placeholder="Start Typing...">
         <button type="button" name="guess-btn" onclick="this.form.submit()">Submit</button>
     </form>
