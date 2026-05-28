a51c10fce1d7bffbe747f21c88906fc284092570
bkoni2008-arch <bkoni2008@gmail.com>
2026-05-28 19:52:56 +0200

    jaja

--- games/background/hatteres.php
+++ games/background/hatteres.php
@@ -172,7 +172,7 @@
                 ?>
                     <tr>
                         <td style="background-color: #1f2937;">
-                            <img class="item-icon" src="../../Items/<?= $g_item['name']; ?>/<?= $g_item['icon']; ?>" alt="Icon">
+                            <img class="icon-img" src="../../Items/<?= $g_item['name']; ?>/<?= $g_item['icon']; ?>" alt="Icon">
                         </td>
                         <td style="background-color: #1f2937;"><?= $g_item['name']; ?></td>
                         
