37571dc5ab6735a6296c39aeb4fa1cd0f8a330c0
Endlessv4-dev <dev.endless.v4@gmail.com>
2026-05-31 23:52:12 +0200

    a

--- leaderboard.php
+++ leaderboard.php
@@ -7,7 +7,26 @@
     <title>Leaderboards</title>
 </head>
 <body>
-<?php require "Functions/nav.php"; ?>
+<nav>
+    <ul>
+        <li><a href="./index.php" class='nav-item' style="color: #4d669b;">Home</a></li>
+        <li><a href="#" class='nav-item'>Leaderboard</a></li>
+        <li><a href="../background/hatteres.php" class='nav-item'>Classic</a></li>
+        <li><a href="../description/leiras.php" class='nav-item'>Description</a></li>
+        <li><a href="../iconguesser/iconguesser.php" class='nav-item'>Icon</a></li>
+        <li><a href="../soundguesser/soundguesser.php" class='nav-item'>Sound</a></li>
+        <li><a href="../stats/statok.php" class='nav-item'>Stats</a></li>
+        <li><a href="../timer/idore.php" class='nav-item'>Time Attack</a></li>
+        <?php if(isset($_COOKIE['userid'])){ ?>
+            <li><a class='reglog' href="../../logout.php" style="color: #ff0000;">Logout</a></li>
+            <?php if($_COOKIE['adminid'] == 1){ ?>
+                <li><a class='admin' href="../../Admin/admin.php" style="color: #ff6060;">Admin</a></li>  
+            <?php } ?>
+        <?php } else { ?>
+            <li><a class='reglog' href="../../reglog.php" style="color: #008000">Login</a></li>
+        <?php } ?>
+    </ul>
+</nav>
 <div class="container">
     <h1 class="home-logo">Leaderboards</h1>
 
