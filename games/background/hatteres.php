8e43881aa72a44f4a814fe8b0d1014b0c694d20d
Endlessv4-dev <dev.endless.v4@gmail.com>
2026-05-31 21:00:32 +0200

    as

--- games/background/hatteres.php
+++ games/background/hatteres.php
@@ -71,7 +71,6 @@
             $error_message = "Selected item does not exist!";
         }
     }
-    require "../../Functions/nav.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -80,7 +79,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="../../css/style.css">
-    <link rel="stylesheet" href="../../css/nav.css">
 </head>
 <body>
 
