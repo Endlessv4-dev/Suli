be2f1576a68ecf13924f88a6afdddbe9b9487f63
Endlessv4-dev <dev.endless.v4@gmail.com>
2026-05-31 23:54:24 +0200

    a

--- leaderboard.php
+++ leaderboard.php
@@ -11,12 +11,12 @@
     <ul>
         <li><a href="./index.php" class='nav-item' style="color: #4d669b;">Home</a></li>
         <li><a href="#" class='nav-item'>Leaderboard</a></li>
-        <li><a href="../background/hatteres.php" class='nav-item'>Classic</a></li>
-        <li><a href="../description/leiras.php" class='nav-item'>Description</a></li>
-        <li><a href="../iconguesser/iconguesser.php" class='nav-item'>Icon</a></li>
-        <li><a href="../soundguesser/soundguesser.php" class='nav-item'>Sound</a></li>
-        <li><a href="../stats/statok.php" class='nav-item'>Stats</a></li>
-        <li><a href="../timer/idore.php" class='nav-item'>Time Attack</a></li>
+        <li><a href="./games/background/hatteres.php" class='nav-item'>Classic</a></li>
+        <li><a href="./games/description/leiras.php" class='nav-item'>Description</a></li>
+        <li><a href="./games/iconguesser/iconguesser.php" class='nav-item'>Icon</a></li>
+        <li><a href="./games/soundguesser/soundguesser.php" class='nav-item'>Sound</a></li>
+        <li><a href="./games/stats/statok.php" class='nav-item'>Stats</a></li>
+        <li><a href="./games/timer/idore.php" class='nav-item'>Time Attack</a></li>
         <?php if(isset($_COOKIE['userid'])){ ?>
             <li><a class='reglog' href="../../logout.php" style="color: #ff0000;">Logout</a></li>
             <?php if($_COOKIE['adminid'] == 1){ ?>
